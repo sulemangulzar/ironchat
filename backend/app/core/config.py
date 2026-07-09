871dfba1b057fc1c6f633659246102e2dd5e533a
Suleman Gulzar <suleman_gulzar@icloud.com>
2026-07-09 14:38:52 +0500

    Fixed Error

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,9 @@
+from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     GROQ_API_KEY : str
-    ALLOWED_ORIGINS : str
+    ALLOWED_ORIGINS : List[str]
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings() #type: ignore
