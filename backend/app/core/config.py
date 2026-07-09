f6ff0391a3ddea2fcb6e97673c74a7d79b0687eb
Suleman Gulzar <suleman_gulzar@icloud.com>
2026-07-09 14:35:27 +0500

    Updated

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     GROQ_API_KEY : str
-
+    ALLOWED_ORIGINS : str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings() #type: ignore
--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes.chat import router as chat_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import settings
+
 app = FastAPI(title="IronChat", version="0.1.0")
+
 
 
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
